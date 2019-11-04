f4d7400c1f84334540307fc3d5edf3e48f48a43d
Anjelica <ambrosio.anjelica@gmail.com>
2019-11-04 11:10:07 -0800

    added results

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def show_form():
 
 @app.route('/fortune_results')
 def show_results():
+    name = request.args.get('name')
+    chara = request.args.get('chara')
+    favebluelion = request.args.get('Fave Blue Lion')
+    sc6character = request.args.get('sc6character')
+    if chara == "Seliph":
+        print("~Seliph's Fortune~")
+        print("You follow the shadows of those before you, and constantly worry")
+        print("about whether you'll be able to live up to the greatness of others.")
     return render_template('results.html')
 
 
